b84308afb588c53d574e3bbf27dc33ca8c0a522a
Flach Dániel <flachdaniel77@gmail.com>
2017-08-07 02:06:36 +0200

    score function is simpler

--- bowling.py
+++ bowling.py
@@ -3,37 +3,28 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+        if frame < 10 and get_value(game[i]) == 10:
+            if game[i] == '/':
+                result += 10 - get_value(game[i - 1]) + (get_value(game[i + 1]))
+            else:
+                if game[i + 2] == '/':
+                    result += 20
+                else:
+                    result += 10 + get_value(game[i + 1]) + get_value(game[i + 2])
+                    in_first_half = True
+                    frame += 1
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    if char in ("xX/"):
         return 10
     elif char == '-':
         return 0
